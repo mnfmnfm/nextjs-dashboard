15f7803db133520a1499cd62927de80c6012b224
Michelle Ferreirae <mnfmnfm@gmail.com>
2024-03-01 15:46:04 -0800

    streaming components

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,15 +1,15 @@
-import { fetchCardData, fetchLatestInvoices, fetchRevenue } from "../lib/data";
-import { Card } from "../ui/dashboard/cards";
-import LatestInvoices from "../ui/dashboard/latest-invoices";
-import RevenueChart from "../ui/dashboard/revenue-chart";
-import { lusitana } from "../ui/fonts";
+import { Suspense } from "react";
+import { fetchCardData } from "../../lib/data";
+import { Card } from "../../ui/dashboard/cards";
+import LatestInvoices from "../../ui/dashboard/latest-invoices";
+import RevenueChart from "../../ui/dashboard/revenue-chart";
+import { lusitana } from "../../ui/fonts";
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from "@/app/ui/skeletons";
 
 export default async function Page() {
-  const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
   const {numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices} = await fetchCardData();
   return <main>
-    <h1 className={`${lusitana.className}`}>
+    <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
       Dashboard
     </h1>
     <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
@@ -19,8 +19,12 @@ export default async function Page() {
       <Card title="Total Customers" value={numberOfCustomers} type="customers" />
     </div>
     <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-      <RevenueChart revenue={revenue} />
-      <LatestInvoices latestInvoices={latestInvoices} />
+      <Suspense fallback={<RevenueChartSkeleton />}>
+        <RevenueChart/>
+      </Suspense>
+      <Suspense fallback={<LatestInvoicesSkeleton />}>
+        <LatestInvoices/>
+      </Suspense>
     </div>
   </main>;
 }
